1be6c5d683e27bc8cfcd050fac286085968e95d8
Lana Lim <lanachloe@g.ucla.edu>
2022-11-29 14:18:51 -0800

    Do not implement deletePost yet.

--- my-app/src/pages/Timeline.js
+++ my-app/src/pages/Timeline.js
@@ -8,11 +8,6 @@ function Timeline({ isAuth }) {
     const [postLists, setPostList] = useState([]);
     const postsCollectionRef = collection(db, "posts");
 
-    const deletePost = async (id) => {
-        const postDoc = doc(db, "posts", id);
-        await deleteDoc(postDoc);
-    };
-
     const displayComments = () => {
         console.log("HELLO") 
     };
@@ -29,9 +24,8 @@ function Timeline({ isAuth }) {
           const data = await getDocs(query(postsCollectionRef, orderBy('timestamp', 'desc')));
           setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
         };
-    
         getPosts();
-    }, [deletePost]);
+    }, []);
     
     return (
         <React.Fragment>
@@ -47,18 +41,6 @@ function Timeline({ isAuth }) {
                     </div>
                     <div className="postTextContainer"> {post.postText} </div>
                     <div className="postTextContainer"> @{post.name} {post.date} {post.value} {post.status} </div>
-                    <div className="deletePost">
-                        {isAuth && post.id === auth.currentUser.uid && (
-                        <button
-                            onClick={() => {
-                            deletePost(post.id);
-                            }}
-                        >
-                            {" "}
-                            &#128465;
-                        </button>
-                        )}
-                    </div>
                 </div>
                 );
             })}
