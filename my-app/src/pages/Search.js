7531dafdbd54ff7306c91744cc1506214b5202b6
Eric.K03 <seoungminkim03@g.ucla.edu>
2022-11-29 18:55:43 -0800

    search_basic

--- my-app/src/pages/Search.js
+++ my-app/src/pages/Search.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
 
 function Search() {
+  const [selectedCategory, setSelectedCategory] = useState("Show All");
+
+  function handleCategoryChange(event) {
+    setSelectedCategory(event.target.value);
+  }
+
   return (
     <React.Fragment>
-      <h1 className="comingSoon">Coming Soon!</h1>
+      <div className="controlArea">
+        <div className="searchBox">
+          <Form.Label htmlFor="inputSearchBar">Search</Form.Label>
+          <Form.Control type="text" aria-describedby="passwordHelpBlock" />
+          <Form.Text>
+            Search for certain USERNAME / DESCRIPTION of an item
+          </Form.Text>
+        </div>
+        <div className="filterBox">
+          <label>Category</label>
+          <Form.Select
+            aria-label="Default select example"
+            onChange={handleCategoryChange}
+          >
+            <option value="Show All">Show All</option>
+            <option value="Clothes">Clothes</option>
+            <option value="Electronics">Electronics</option>
+            <option value="Others">Others</option>
+          </Form.Select>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
