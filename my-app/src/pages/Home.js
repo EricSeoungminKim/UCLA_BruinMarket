5957c6911cfd0b2c8d6d227f8496f830728ad529
Emily Nham <emilynham@Emilys-MacBook-Air.local>
2022-11-30 03:38:44 -0800

    Update Home page. Implement sticky navigation bar.

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -11,15 +11,36 @@ function Home() {
         className="homepageBanner"
         style={{ backgroundImage: `url(${roycehall})` }}
       >
-        <div className="homepageBannerTitle">
-          <h1>Bruin Marketplace</h1>
-          <NavLink to="/aboutus" className="linkmenuItem">
+        <View className="homepageBannerTitle">
+          <Text style={styles.title}>Welcome to{"\n"}Bruin Marketplace!</Text>
+          <NavLink to="/aboutus" className="linkmenuItem" style={styles.button}>
             Learn more ➜
           </NavLink>
-        </div>
+        </View>
       </div>
     </React.Fragment>
   );
 }
+
+const styles=StyleSheet.create({
+
+  title:{
+    fontSize: 60,
+    fontWeight: 500,
+    marginTop: 40,
+    marginRight: 40,
+    alignSelf: 'flex-end',
+    fontFamily: "LoveloBlack",
+    color: "white",
+    textAlign: "right"
+  },
+
+  button: {
+    margin: 20,
+    marginRight: 40,
+    alignSelf: "flex-end"
+  }
+
+})
 
 export default Home;
